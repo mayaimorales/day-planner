17cec1e059432a71e987679bd64c7e6aabfff2c8
Maya Morales <mayainomorales@gmail.com>
2022-06-08 21:41:58 -0700

    add schedule

---
+++ script.js
@@ -0,0 +1,50 @@
+var today = moment();
+
+var times = $(".container");
+var date = $("currentDay");
+
+var currentHour = today.hour();
+
+var currentDay;
+
+var morning;
+
+var schedule = [
+    {
+        time: 9,
+        message: "",
+    },
+    {
+        time: 10,
+        message: "",
+    },
+    {
+        time: 11,
+        message: "",
+    },
+    {
+        time: 12,
+        message: "",
+    },
+    {
+        time: 1,
+        message: "",
+    },
+    {
+        time: 2,
+        message: "",
+    },{
+        time: 3,
+        message: "",
+    },{
+        time: 4,
+        message: "",
+    },{
+        time: 5,
+        message: "",
+    }
+];
+
+function Day(){
+    
+}
