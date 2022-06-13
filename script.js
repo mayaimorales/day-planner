698818e7f9ec7c70d935a183ceb8f4bc55a47d65
Maya Morales <mayainomorales@gmail.com>
2022-06-13 00:56:47 -0700

    add function that will build day

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ var currentDay;
 
 var morning;
 
+//schedule definition for each day, messages are empty
 var schedule = [
     {
         time: 9,
@@ -49,6 +50,7 @@ var schedule = [
 ];
 
 function Day(){
+    //will distinguish hour in the day and if it is morning or evening
     if (currentHour >= 12){
         morning = false;
         if (currentHour > 12 ){
@@ -57,4 +59,37 @@ function Day(){
     }else{
         morning = true;
     }
+    times.addClass('mb-3');
+    //goes through necessary hours for work day
+    for (var i = 9; i < 12; i ++){
+        Hour(i, true);
+    }
+    Hour(12, false);
+    for(var i = 12; i < 6; i++){
+        Hour(i, false);
+    }
+
+    //sets today's date format
+    date.text(today.format("MMM Do, YYYY"));
+
+    currentDay = JSON.parse(localStorage.getItem('schedule'));
+
+    if(currentDay == null){
+        currentDay = schedule;
+    }
+
+    for (i=0; i<times.children().length; i++){
+        times.children().eq(i).children().eq(1).children().first.val(currentDay[i].message);
+    }
+
+};
+
+function Hour(hour, am){
+    var div = $('<div>');
+    div.addClass('time-block');
+    div.addClass('row');
+    div.addClass('col-sm-12');
+
+    var p = $('<p>');
+    p.addClass('hour');
 }
